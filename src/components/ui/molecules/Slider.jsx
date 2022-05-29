9178be9603c4317c153ced8078e6dec89220ef16
gbTechh <8kdots@gmail.com>
2022-05-29 11:18:18 -0500

    slick nav css

--- src/components/ui/molecules/Slider.jsx
+++ src/components/ui/molecules/Slider.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+
 import { Div, Typography } from "../../../../uixlibrary/components/exports/components";
 import { css } from "@emotion/react";
 
